29ec15fac19e578538e8010c3bbc7446ab7d79de
Roman Lysenko <skiptomy55@gmail.com>
2020-05-31 19:08:54 +0000

    Updates

---
+++ precache-manifest.f09000032c772d97b317170d3f63d72a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f403cda83f52a72f4ac55e9a63699654",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "38f6ac6f9357db6e80cb",
+    "url": "/goit-react-hw-04-movies/static/css/main.59c0526c.chunk.css"
+  },
+  {
+    "revision": "0e5bf4a4f5a2226c69fb",
+    "url": "/goit-react-hw-04-movies/static/js/2.74a858d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/2.74a858d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f6ac6f9357db6e80cb",
+    "url": "/goit-react-hw-04-movies/static/js/main.ce7f960f.chunk.js"
+  },
+  {
+    "revision": "298fdbd9c05d087ad24d",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.a5f325cd.js"
+  }
+]);
